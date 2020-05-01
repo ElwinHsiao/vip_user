9c061a3686ac51da84a70fa5ef2c41c38e54e501
elwinxiao <elwinxiao@tencent.com>
2020-05-01 18:36:19 +0800

    implemnt android ui; add djinni support code

--- android/app/src/main/java/per/elwin/vipuser/MainActivity.kt
+++ android/app/src/main/java/per/elwin/vipuser/MainActivity.kt
@@ -2,17 +2,66 @@ package per.elwin.vipuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+//        sample_text.text = stringFromJNI()
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+        verticalLayout {
+            padding = 16
+            val userName = editText {
+                hint = "enter you user name"
+            }
+            val password = editText() {
+                hint = "enter you password"
+                transformationMethod = PasswordTransformationMethod.getInstance()
+                //inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+            button {
+                text = "Login"
+                onClick {
+                    perform(false, userName.text.toString(), password.text.toString())
+                }
+            }
+            button {
+                text = "Register"
+                onClick {
+                    perform(true, userName.text.toString(), password.text.toString())
+                }
+            }
+        }
     }
+
+
+
+    private fun perform(isRegister: Boolean, userName: String?, password: String?) {
+        if (userName == null || password == null) {
+            Log.e(LOG_TAG, "name and password can't be empty");
+            return
+        }
+        if (userName.isEmpty() || password.isEmpty()) {
+            Log.e(LOG_TAG, "name and password can't be empty");
+            return
+        }
+
+
+    }
+
+
+    private fun handleAccountResponse(isRegister: Boolean) {
+
+    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
@@ -21,9 +70,12 @@ class MainActivity : AppCompatActivity() {
     external fun stringFromJNI(): String
 
     companion object {
+        val LOG_TAG = "VipUser"
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
         }
     }
+
 }
+
